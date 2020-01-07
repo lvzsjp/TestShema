bdef35f8e1d9f544d109d660c8a923fc67240a60
lvzs <lvzsjp@yahoo.co.jp>
2020-01-08 01:06:04 +0900

    2020.01.07

--- TestShema/ViewController.m
+++ TestShema/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Shema/Shema.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    ShemaContainer *shema = [[ShemaContainer alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:shema];
+    
+    [shema LoadBgImage:[UIImage imageNamed:@"sample2.jpg"]];
 }
 
 
